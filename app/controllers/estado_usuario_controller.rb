ae549a956a473193aac7968aa606a4b552a5e710
Pepe Valdivia <jvaldivia@softweb.pe>
2017-10-23 14:56:18 -0500

    Query usando connection.exec_query

--- app/controllers/estado_usuario_controller.rb
+++ app/controllers/estado_usuario_controller.rb
@@ -5,4 +5,13 @@ class EstadoUsuarioController < ApplicationController
     def listar
         render :plain =>  EstadoUsuario.all.to_json
     end
+
+    def combinado
+        query = '
+            SELECT U.id, U.usuario, EU.nombre  FROM 
+            usuarios U INNER JOIN estado_usuarios EU 
+            ON U.estado_usuario_id = EU.id'
+        results = ActiveRecord::Base.connection.exec_query(query)
+        render :plain =>  results.to_json
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-   get 'estado_usuario' => 'estado_usuario#index'
-   get 'estado_usuario/listar' => 'estado_usuario#listar'
-
+    get 'estado_usuario' => 'estado_usuario#index'
+    get 'estado_usuario/listar' => 'estado_usuario#listar'
+    get 'estado_usuario/combinado' => 'estado_usuario#combinado'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
